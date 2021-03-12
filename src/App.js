a6c2c16fbe2af6afff71735f570356401dd3dab2
Tatyana Tanana <tananatayna@gmail.com>
2021-03-13 00:18:54 +0300

    homework-3: displayed pokemon cards

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 import Footer from "./components/Footer";
+import PokemonCard from "./components/PokemonCard";
 
 import bg1 from './assets/bg1.jpg';
 import bg2 from './assets/bg2.jpg';
 // import bg3 from './assets/bg3.jpg';
 
+import './App.css';
+
+import POKEMONS from './data/pokemons.json';
+
 const App = () => {
     return (
         <>
             <Header
-                title="This is title"
-                descr="This is Description!"
+                title="Pokemon Game"
+                descr="This is a simple triple triad card game :)"
             />
             <Layout
-                title="This is title for Layout 1"
-                descr="This is Description for Layout 1!"
+                title="Rules"
                 urlBg={bg1}
-            />
+            >
+                <p>In the game two players face off against one another, one side playing as "blue", the other as "red"
+                    on a 3x3 grid.</p>
+                <p>Each player has five cards in a hand and the aim is to capture the opponent's cards by turning them
+                    into the player's own color of red or blue.</p>
+                <p>To win, a majority of the total ten cards played (including the one card that is not placed on the
+                    board) must be of the player's card color. To do this, the player must capture cards by placing a
+                    card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch
+                    will be compared. If the rank of the opponent's card is higher than the player's card, the player's
+                    card will be captured and turned into the opponent's color. If the player's rank is higher, the
+                    opponent's card will be captured and changed into the player's color instead.</p>
+            </Layout>
             <Layout
-                title="This is title for Layout 2"
+                title="Cards"
                 colorBg="#154c79"
-            />
+            >
+                <div className="flex">
+                    {
+                        POKEMONS.map(item => <PokemonCard {...item} />)
+                    }
+                </div>
+            </Layout>
             <Layout
-                title="This is title for Layout 3"
-                descr="This is Description for Layout 3!"
+                title="The end"
                 urlBg={bg2}
-            />
+            >
+                <p>See you soon!</p>
+                <br />
+                <br />
+            </Layout>
+
             <Footer />
         </>
     );
